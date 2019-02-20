f343ce7c7618f6de36a59237317d683687713ace
Andy0414 <pjh8667@naver.com>
2019-02-20 22:55:12 +0900

    UPDATE infinity number list

--- lib/main.dart
+++ lib/main.dart
@@ -6,57 +6,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First Project',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'My First Flutter'),
+      title: '무한 스크롤',
+      home: RandomWords(), // 액티비티 불러오기
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <int>[]; // WordPair (단어) 배열
+  final _biggerFont = const TextStyle(fontSize: 18.0); // 폰트 사이즈 선언
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0), // 패딩
+        itemBuilder: (context, i) { // 안스에서 어댑터 안에 getView와 같은 역할
+          if (i.isOdd) return Divider(); // 홀수번째는 구분선 추가
+          final index = i ~/ 2; // 나눈값 정수 반환 (구분선 제외를 위한 나누기)
+          if (index >= _suggestions.length) { // 스크롤량이 절반이상일시 단어추가
+            _suggestions.add(index);  // 번호를 배열에 추가
+          }
+          return _buildRow(_suggestions[index]); // 리스트 타일 생성
+        });
   }
-
+  Widget _buildRow(int pair) { // 리스트 타일 생성 함수
+    return ListTile(
+      title: Text( // 타이틀
+        pair.toString(),
+        style: _biggerFont,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Infinite Number List'), // 앱바 타이틀
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      body: _buildSuggestions(), // 리스트뷰 반환 함수
     );
   }
+}
+class RandomWords extends StatefulWidget { // StatefulWidget (상태 변경 가능)
+  @override
+  RandomWordsState createState() => new RandomWordsState(); // 이 위젯이 생성시 RandomWordsState를 불러옴
 }
